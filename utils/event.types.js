00ffc80612ddb7845e81912bb59ffde888ea4f3b
Rene0115 <enere0115@gmail.com>
2023-07-21 21:14:53 +0100

    signup and login updated

---
+++ utils/event.types.js
@@ -0,0 +1,28 @@
+const event_types = [
+  "Event venues",
+  "Event planner",
+  "Bar services and beverages",
+  "Photography",
+  "Beauty professional",
+  "Fashion designers & Stylists",
+  "Decorators",
+  "Videographer",
+  "Clothing and accessories",
+  "Event staffs",
+  "Caterer",
+  "Baker",
+  "Printing service",
+  "Event rental",
+  "Favours & Gifts",
+  "Music & Entertainment",
+  "Lighting and AV",
+  "Officiant & Speaker",
+  "Dancing instructor",
+  "Health & Fitness",
+  "Accomodation",
+  "Transportation service",
+  "Model",
+  "Social media influencer"
+];
+
+export default event_types
