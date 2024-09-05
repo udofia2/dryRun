96845f743f56ba6fb181cfc475488647f0ec90f9
Sam <s.olubode@alustudent.com>
2024-09-05 12:05:41 +0100

    update(auth): class-validate SocialAuthDto

--- src/auth/dto/google-auth.dto.ts
+++ src/auth/dto/google-auth.dto.ts
@@ -1,8 +1,22 @@
+import { IsOptional, IsString } from "class-validator";
+
 export class SocialAuthDto {
+  @IsString()
   provider: string;
+
+  @IsString()
   providerId: string;
+
+  @IsString()
   email: string;
+
+  @IsString()
   firstname: string;
+
+  @IsString()
   lastname: string;
+
+  @IsOptional()
+  @IsString()
   avatar?: string;
 }
